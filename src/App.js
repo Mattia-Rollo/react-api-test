80d151cce41075cfbf709b82848d741d6f48e3b9
mattia.rollo <ste.tia@hotmail.it>
2023-02-27 18:42:07 +0100

    add call api for user info

--- src/App.js
+++ src/App.js
@@ -4,27 +4,50 @@ import { useState } from "react";
 
 function App() {
   const [cards, setCards] = useState([]);
-  const img = "https://image.tmdb.org/t/p/w500/";
+  // const img = "https://image.tmdb.org/t/p/w500/";
+  const id = 1;
   const getMovie = async () => {
     try {
       const response = await fetch(
-        "https://api.themoviedb.org/3/movie/550?api_key=8ace785dd1f96b68334521629f5dadaf&language=it"
-      );
-      // .then((res) => console.log(res))
-      // .then((data) => console.log(data));
+        `https://jsonplaceholder.typicode.com/posts/${id}`
+      )
+      // .then((res) => res.json())
+      // .then((data) => console.log("post:" + data.body, "userId:" + data.userId))
+      
+
+      // console.log(response.json());
+      // const response2 = await fetch(
+      //   `https://jsonplaceholder.typicode.com/users/${response.data.userId}`
+      // )
+      // .then((res) => {res.json(); console.log(res)})
+      // .then((data) => console.log("User: " + data))
+      // .catch(error => console.error(error));
       // console.log(like);
+
       if (!response.ok) {
         throw new Error("Data coud not be fetched!");
       } else {
-        const data = await response.json();
-        console.log(data);
-        setCards((prevCards) => {
-          return [...prevCards, { ...data, id: cards.length }];
-        });
+          const data = await response.json();
+          const resUser = await fetch(
+          `https://jsonplaceholder.typicode.com/users/${data.userId}`
+          )
+
+        if(!resUser.ok){
+          throw new Error("Data coud not be fetched!");
+        }else{
+          const dataUser = await resUser.json();
+
+          console.log(data, dataUser);
+          setCards((prevCards) => {
+            return [...prevCards, {...data, id: cards.length + 1, dataUser}];
+          });
+          console.log(cards)
+        }
       }
     } catch (error) {
       console.log("non ricevo dati");
     }
+    
   };
 
   return (
@@ -46,22 +69,7 @@ function App() {
         <ul>
           {cards
             ? cards.map((card) => (
-                <>
-                  {/* <li key={card.id}>
-                    {Object.entries(item).map(
-                    ([key, value]) => `${key}: ${value}`
-                  )}
-                    <img
-                      src={
-                        "https://image.tmdb.org/t/p/w500/" + card.backdrop_path
-                      }
-                      alt="ok"
-                    />
-                    <div>{card.overview}</div>
-                    <div>{card.Actors}</div>
-                  </li> */}
-                  <Card key={card.id} card={card} />
-                </>
+                <Card key={card.id} card={card} />
               ))
             : "no item"}
         </ul>
@@ -73,11 +81,13 @@ function App() {
 function Card({ card }) {
   return (
     <>
-      <img
+      {/* <img
         src={"https://image.tmdb.org/t/p/w500/" + card.backdrop_path}
         alt="ok"
-      />
+      /> */}
       <h3>{card.title}</h3>
+      <h3>{card.dataUser.name}</h3>
+      <div>card id: {card.id}</div>
       <div>{card.overview}</div>
     </>
   );
