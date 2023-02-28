b2198c4d6cb359ca053dbe664a6280bcb727b8d5
mattia.rollo <ste.tia@hotmail.it>
2023-02-28 11:36:42 +0100

    style  + card skeleton bootstrap

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function App() {
 
           console.log(data, dataUser);
           setCards((prevCards) => {
-            return [...prevCards, {...data,  ...dataUser,id: cards.length + 1}];
+            return [...prevCards, {...data, ...dataUser,id: cards.length + 1}];
           });
           console.log(cards)
         }
@@ -54,34 +54,81 @@ function App() {
 
   return (
     <div className="App">
+        <h1>Doppia chiamata Api Con Fetch + Async Await + trycatch</h1>
       <header className="App-header">
-       
-        <button onClick={getMovie} className="btn btn-lg btn-primary m-3">premi</button>
+
+        
         <div className="container">
           <div className="row row-cols-4 g-2">
-            {/* <div className="card" aria-hidden="true">
-              <img src="..." className="card-img-top" alt="..." />
-              <div className="card-body">
-                <h5 className="card-title placeholder-glow">
-                  <span className="placeholder col-6"></span>
-                </h5>
-                <p className="card-text placeholder-glow">
-                  <span className="placeholder col-7"></span>
-                  <span className="placeholder col-4"></span>
-                  <span className="placeholder col-4"></span>
-                  <span className="placeholder col-6"></span>
-                  <span className="placeholder col-8"></span>
-                </p>
-                <a href="#" tabIndex="-1" className="btn btn-primary disabled placeholder col-6"></a>
-              </div>
-            </div> */}
-            {cards
+            
+            {cards.length
               ? cards.map((card) => (
                 <Card key={card.id} card={card} />
                 ))
-                : "no item"}
+                  : (
+                  <>
+                  <div className="col">
+                    <div className="card bg-dark" aria-hidden="true">
+                      <div id="skeleton" className="card-img-top"></div>
+                      {/* <img src="..." className="card-img-top" alt="..." /> */}
+                      <div className="card-body">
+                        <h5 className="card-title placeholder-glow">
+                          <span className="placeholder rounded-1 col-6"></span>
+                        </h5>
+                        <p className="card-text placeholder-glow">
+                          <span className="placeholder rounded-1 col-12"></span>
+                          {/* <span className="placeholder col-4"></span> */}
+                          <span className="placeholder rounded-1 col-10"></span>
+                          {/* <span className="placeholder col-6"></span> */}
+                          <span className="placeholder rounded-1 col-8"></span>
+                        </p>
+                        {/* <a href="#" tabIndex="-1" className="btn btn-primary disabled placeholder col-6"></a> */}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="card bg-dark" aria-hidden="true">
+                      <div id="skeleton" className="card-img-top"></div>
+                      {/* <img src="..." className="card-img-top" alt="..." /> */}
+                      <div className="card-body">
+                        <h5 className="card-title placeholder-glow">
+                          <span className="placeholder col-6"></span>
+                        </h5>
+                        <p className="card-text placeholder-glow">
+                          <span className="placeholder col-7"></span>
+                          <span className="placeholder col-4"></span>
+                          <span className="placeholder col-4"></span>
+                          <span className="placeholder col-6"></span>
+                          <span className="placeholder col-8"></span>
+                        </p>
+                        {/* <a href="#" tabIndex="-1" className="btn btn-primary disabled placeholder col-6"></a> */}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col">
+                    <div className="card bg-dark" aria-hidden="true">
+                      <div id="skeleton" className="card-img-top"></div>
+                      {/* <img src="..." className="card-img-top" alt="..." /> */}
+                      <div className="card-body">
+                        <h5 className="card-title placeholder-glow">
+                          <span className="placeholder col-6"></span>
+                        </h5>
+                        <p className="card-text placeholder-glow">
+                          <span className="placeholder col-7"></span>
+                          <span className="placeholder col-4"></span>
+                          <span className="placeholder col-4"></span>
+                          <span className="placeholder col-6"></span>
+                          <span className="placeholder col-8"></span>
+                        </p>
+                        {/* <a href="#" tabIndex="-1" className="btn btn-primary disabled placeholder col-6"></a> */}
+                      </div>
+                    </div>
+                  </div>
+                  </>
+              )}
           </div>
         </div>
+        <button onClick={getMovie} className="btn btn-lg btn-primary m-3">premi</button>
       </header>
     </div>
   );
@@ -94,11 +141,13 @@ function Card({ card }) {
         src={"https://image.tmdb.org/t/p/w500/" + card.backdrop_path}
         alt="ok"
       /> */}
-      <div className="col bg-gradient">
-        <h3 >{card.title}</h3>
-        <h5 className="text-secondary">Autor: {card.name}</h5>
-        {/* <div>card id: {card.id}</div> */}
-        <p>{card.body}</p>
+      <div className="col">
+        <div className="card bg-dark bg-gradient">
+          <h3 className="p-2">{card.title}</h3>
+          <div className="text-secondary p-2">Autor: {card.name}</div>
+          {/* <div>card id: {card.id}</div> */}
+          <div className="card-body">{card.body}</div>
+        </div>
       </div>
     </>
   );
